195a5783554fd23cae0dc4dc78b2c8098f4e062c
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-04-05 14:46:07 +0000

    add new routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4001;
@@ -11,10 +11,58 @@ const test_data=[
     { id:3, nom:"Lucas", age:35},
     { id:4, nom:"Sophia", age:28},
     { id:5, nom:"Liam", age:32},
-]
-app.get('/', (req, res) => {
+];
+
+// Route to get all data
+app.get('/data', (req, res) => {
     res.json(test_data);
-  });
-app.listen(PORT,()=>{
-    console.log(`Visit : http://127.0.0.1:${PORT}`)
+});
+
+// Route to get data by ID
+app.get('/data/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const data = test_data.find(item => item.id === id);
+    if (data) {
+        res.json(data);
+    } else {
+        res.status(404).send('Data not found');
+    }
+});
+
+app.get('/data/first/:count', (req, res) => {
+    const count = parseInt(req.params.count);
+    const firstItems = test_data.slice(0, count);
+    if (firstItems.length > 0) {
+        res.json(firstItems);
+    } else {
+        res.status(404).send('No data found');
+    }
+});
+
+app.get('/data/last/:count', (req, res) => {
+    const count = parseInt(req.params.count);
+    const lastItems = test_data.slice(-count);
+    if (lastItems.length > 0) {
+        res.json(lastItems);
+    } else {
+        res.status(404).send('No data found');
+    }
+});
+
+app.get('/data/sorted-by-age/:order', (req, res) => {
+    const order = req.params.order.toLowerCase();
+    if (order === 'asc') {
+        const sortedData = test_data.slice().sort((a, b) => a.age - b.age);
+        res.json(sortedData);
+    } else if (order === 'desc') {
+        const sortedData = test_data.slice().sort((a, b) => b.age - a.age);
+        res.json(sortedData);
+    } else {
+        res.status(400).send('Invalid order parameter. Use "asc" or "desc".');
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Visit : http://127.0.0.1:${PORT}`);
 });
